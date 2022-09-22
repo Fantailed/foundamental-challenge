541d7abe9791b822a2b5cf919f939d63634ffbb8
Conny Lin <cornelia.lin@tum.de>
2022-09-22 20:27:35 +0200

    Format code

--- frontend/src/lib/company_deals.ts
+++ frontend/src/lib/company_deals.ts
@@ -2,16 +2,6 @@ import useSWR from "swr"
 import { DataTableData, DataTableFormatSpec } from "../components/data_table"
 
 
-const url = 'http://localhost:20002/api/company_deals'
-const fetcher = (url: string) => fetch(url).then(res => res.json())
-
-export const companyDealFormat: DataTableFormatSpec = [
-    ['Company Name', 'name'],
-    ['Funding Round', 'funding_round'],
-    ['Funding Amount', 'funding_amount'],
-    ['Deal Date', 'date']
-]
-
 export interface CompanyDealData extends DataTableData {
     id: number,
     date: string,
@@ -19,6 +9,10 @@ export interface CompanyDealData extends DataTableData {
     funding_round: string,
     name: string
 }
+
+
+const url = 'http://localhost:20002/api/company_deals'
+const fetcher = (url: string) => fetch(url).then(res => res.json())
 
 export function getCompanyDeals(): {
     data: CompanyDealData[],
@@ -33,3 +27,10 @@ export function getCompanyDeals(): {
         isError: error
     }
 }
+
+export const companyDealFormat: DataTableFormatSpec = [
+    ['Company Name', 'name'],
+    ['Funding Round', 'funding_round'],
+    ['Funding Amount', 'funding_amount'],
+    ['Deal Date', 'date']
+]
