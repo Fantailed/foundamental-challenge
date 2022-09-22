1c795d8a8b716697aa9eca96b9fcc38fc6d808b3
Conny Lin <cornelia.lin@tum.de>
2022-09-22 20:49:46 +0200

    Incorporate elements of original index to make more pretty

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 import DataTable from '../components/data_table'
 import { companyDealFormat, getCompanyDeals } from '../lib/company_deals'
 
-const apiUrl = 'http://localhost:20002/api/company_deals'
 
 const Home: NextPage = () => {
 
@@ -21,14 +21,29 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Companies & Deals
+          Cool App Name
         </h1>
+
+        <p className={styles.description}>Overview of Companies and Deals</p>
         {
-          isLoading? <p>Loading...</p>:
-          !data? <p>No deal data.</p> :
+          isLoading? <p className={styles.description}>Loading...</p>:
+          !data? <p className={styles.description}>No deal data.</p> : 
           <DataTable dataFmt={companyDealFormat} data={data} />
         }
       </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
     </div>
   )
 }
