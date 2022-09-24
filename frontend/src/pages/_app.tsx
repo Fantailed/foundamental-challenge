fea213b9dae33b68e9c6c50e503452ac43f2bb7b
Conny Lin <cornelia.lin@tum.de>
2022-09-24 11:25:04 +0200

    Use default primereact theme

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,4 +1,9 @@
 import '../styles/globals.css'
+
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+ 
 import type { AppProps } from 'next/app'
 
 import Layout from '../components/layout'
