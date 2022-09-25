58ed18af1bd3559dc4ef00cb9a0e5dffc778a824
Conny Lin <cornelia.lin@tum.de>
2022-09-25 15:15:35 +0200

    Use state for page title

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import Image from 'next/image'
 
 import styles from "./layout.module.css"
 import HeaderBar from "./headerbar";
@@ -8,14 +7,20 @@ import SideNavigation from "./sidenavigation";
 import { route2pageName } from "../lib/pageconfig";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+    const router = useRouter()
+    const [ pageTitle, setPageTitle ] = useState(route2pageName(router.pathname));
+
+    useEffect(() => {
+        setPageTitle(route2pageName(router.pathname));
+    }, [router.pathname]);
+
     return (
         <>
             {/* Sidebar */}
             <SideNavigation />
 
             {/* Header */}
-            {/* TODO: Use state or something to avoid recalculation at every render */}
-            <HeaderBar pageTitle={route2pageName(useRouter().pathname)} />
+            <HeaderBar pageTitle={pageTitle} />
 
             <main className={styles.main}>{children}</main>
 
