25eddcdb9bc76abe20bb3683a09e97dd2a302c80
Conny Lin <cornelia.lin@tum.de>
2022-09-25 15:02:13 +0200

    Put overview table in card

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
-import DataTable from '../components/data_table'
-import { companyDealFormat, getCompanyDeals } from '../lib/company_deals'
 import CompanyDealsDataTable from '../components/companydeals_datatable'
+import { Card } from 'primereact/card'
 
 
 const Home: NextPage = () => {
@@ -17,7 +14,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <CompanyDealsDataTable />
+      <Card>
+        <CompanyDealsDataTable />
+      </Card>
     </>
   )
 }
