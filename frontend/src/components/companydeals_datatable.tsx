8a6a1bc834ca84f6ac8bff6f39e61b9e43af86f0
Conny Lin <cornelia.lin@tum.de>
2022-09-25 14:57:43 +0200

    Add global search and pagination to overview

--- frontend/src/components/companydeals_datatable.tsx
+++ frontend/src/components/companydeals_datatable.tsx
@@ -1,7 +1,9 @@
 import { DataTable } from 'primereact/datatable';
 import { Column, ColumnBodyOptions } from 'primereact/column';
 import { getCompanyDeals } from '../lib/api_requests';
-import { moneyFormatter } from './dbdatatable';
+import { dataTableGlobalSearchHeader, moneyFormatter, paginatorLeft, paginatorRight } from './dbdatatable';
+import { useState } from 'react';
+import { FilterMatchMode } from 'primereact/api';
 
 
 export default function CompanyDealsDataTable() {
@@ -14,12 +16,30 @@ export default function CompanyDealsDataTable() {
 
     const { data, isLoading } = getCompanyDeals();
 
+    // Global search
+    const [filters, setFilters] = useState({
+        'global': { value: null, matchMode: FilterMatchMode.CONTAINS }
+    });
+
     const dynamicColumns = columns.map((col,i) => {
         return <Column key={col.field} field={col.field} header={col.header} body={col.body} sortable/>;
     });
 
     return (
-        <DataTable value={data} sortField="date" sortOrder={-1} scrollable scrollHeight="500px" loading={isLoading}>
+        <DataTable
+            value={data} sortField="date" sortOrder={-1}
+            header={() => dataTableGlobalSearchHeader(filters, setFilters)}
+            filters={filters} onFilter={(e: any) => setFilters(e.filters)}
+            responsiveLayout="scroll"
+            loading={isLoading}
+            paginator
+            paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
+            currentPageReportTemplate="Showing {first} to {last} of {totalRecords}"
+            rows={10} rowsPerPageOptions={[10, 20, 50]}
+            paginatorLeft={paginatorLeft} paginatorRight={paginatorRight}
+            autoLayout={true}
+            size="small"
+        >
             {dynamicColumns}
         </DataTable>
     );
