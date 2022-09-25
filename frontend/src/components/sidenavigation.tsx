4ad41009aa2f8c023945fbda912551ea71edfbcb
Conny Lin <cornelia.lin@tum.de>
2022-09-25 15:51:47 +0200

    Highlight current page in sidenav

--- frontend/src/components/sidenavigation.tsx
+++ frontend/src/components/sidenavigation.tsx
@@ -3,21 +3,35 @@ import Link from 'next/link';
 
 import styles from "./sidenavigation.module.css"
 import { route2pageName } from '../lib/pageconfig';
+import { Divider } from 'primereact/divider';
+import { useRouter } from 'next/router';
 
 export default function SideNavigation() {
+    const router = useRouter();
+    const currentRoute = router.pathname;
+
     return(
         <div className={styles.sidenav}>
             <div className={styles.appLogo}>
                 <Image src="/vercel_white.svg" alt="App Logo" width={130} height={24} />
             </div>
 
-            <Link href="/">{route2pageName('/')}</Link>
+            <Link href="/">
+                <a className={currentRoute === '/'? styles.active: undefined}>Overview</a>
+            </Link>
 
-            <hr />
+            {/* ------------------------------------------------------------ */}
+            <Divider align="center" style={{ width: '90%', marginLeft: '5%' }}/>
 
             <p>Database</p>
-            <Link href="/db/companies">Companies</Link>
-            <Link href="/db/deals">Deals</Link>
+
+            <Link href="/db/companies">
+                <a className={currentRoute === '/db/companies'? styles.active: undefined}>Companies</a>
+            </Link>
+
+            <Link href="/db/deals">
+                <a className={currentRoute === '/db/deals'? styles.active: undefined}>Deals</a>
+            </Link>
         </div>
     );
 }
