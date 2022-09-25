874b62426d9f3b11008cd4236124a0920c8324f8
Conny Lin <cornelia.lin@tum.de>
2022-09-25 12:13:06 +0200

    Implement Deals with simple columns

---
+++ frontend/src/components/deals_datatable.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { DataTableRowEditCompleteParams } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { FilterMatchMode } from 'primereact/api';
+
+import { getDeals, patchDeal } from '../lib/api_requests';
+import DBDataTable, { dateTimeEditor, textEditor } from './dbdatatable';
+
+
+export default function DealsDataTable() {
+    const { data: dealData, isLoading, mutate } = getDeals();
+
+    // Row Editing
+    const onRowEditComplete = (e: DataTableRowEditCompleteParams) => {
+        let { index, newData } = e;
+        dealData[index] = newData;
+
+        // API PATCH request
+        patchDeal(newData);
+        // Optimistically update state in client
+        mutate(dealData);
+    }
+
+    // Global search
+    const [filters, setFilters] = useState({
+        'global': { value: null, matchMode: FilterMatchMode.CONTAINS }
+    });
+
+
+    return (
+        <DBDataTable
+            value={dealData}
+            onRowEditComplete={onRowEditComplete}
+            loading={isLoading}
+            filters={filters}
+            setFilters={setFilters}
+        >
+            <Column key='id' field='id' header='ID' />
+            <Column key='date' field='date' header='Date' editor={(options) => dateTimeEditor(options)} />
+            <Column key='funding_round' field='funding_round' header='Founding Round' editor={(options) => textEditor(options)} />
+            <Column rowEditor headerStyle={{ width: '10%', minWidth: '8rem' }} bodyStyle={{ textAlign: 'center' }} />
+        </DBDataTable>
+    );
+}
--- frontend/src/pages/db/deals.tsx
+++ frontend/src/pages/db/deals.tsx
@@ -1,11 +1,17 @@
 import type { NextPage } from "next"
+import { Card } from "primereact/card";
+import DealsDataTable from "../../components/deals_datatable";
 
 const Deals: NextPage = () => {
-    return (
-        <>
-        <p>Some deal</p>
-        </>
-    );
+    return (<>
+        <Card className='infocard' title='Caution!'>
+            <p> Modifications on this page are persisted in the database! </p>
+        </Card>
+
+        <Card>
+            <DealsDataTable />
+        </Card>
+    </>);
 }
 
 export default Deals
